f8a542ac23748b9f3a2c21179b8945c0e030c6a8
rajeev-marwah-sp <108899072+rajeev-marwah-sp@users.noreply.github.com>
2023-12-22 18:09:06 -0600

    Update uncorrelatedaccount.py

--- uncorrelatedaccount.py
+++ uncorrelatedaccount.py
@@ -1,6 +1,7 @@
 import csv
 import requests
 import subprocess
+#Read the CSv file
 def read_csv(csv_file_path):
 
     with open(csv_file_path, 'r', encoding='utf-8-sig') as csv_file:
@@ -8,13 +9,10 @@ def read_csv(csv_file_path):
         # Assuming the CSV file has only one row of data
         row = (csv_reader, {})
         for line in csv_reader:
-            print(line)
-        return(line)
+         return(line)
+#Calling the uncorrelated account
 def uncorrelatedaccounts(api_url,token):
-
     tokenId = token.rstrip("\n")
-    print(tokenId)
-    print(api_url)
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -23,39 +21,32 @@ def uncorrelatedaccounts(api_url,token):
     response = requests.get(api_url, headers=headers)
     print(response.status_code)
     print(response.text)
-
+#Calling the get token function, get_token script should be in the same folder
 def get_token():
     #print("In get token")
     try:
      completed_process=subprocess.run(['python','gettoken.py'],check=True,capture_output=True,text=True)
      if completed_process.returncode == 0:
-         #print("Execution successful.")
-         #print("Output:")
-         #print(completed_process.stdout)
          return(completed_process.stdout)
      else:
          print(f"Error: Failed to execute")
-         print("Error output:")
          print(completed_process.stderr)
 
 
     except subprocess.CalledProcessError as e:
         print("Unable to get the token from get_token.py: {e}")
         return None
-
+#Main Function
 if __name__ == "__main__":
 
     token = get_token()
-    print(token)
 
     # Replace 'input.csv' with your actual CSV file path
     csv_file_path = "/Users/rajeev.marwah/Documents/Customer Projects/ES Cases/Python script/sourceoutput.csv"
     sourceId=read_csv(csv_file_path)
-    print(sourceId)
+
     srcId=sourceId.get("sourcename", "")
-    Id=str(srcId)
-    print(srcId)
     # Replace the URL with your actual API endpoint
     api_url = f'https://company5780-poc.api.identitynow-demo.com/v3/accounts?filters=uncorrelated%20eq%20true%20and%20sourceId%20eq%20\"{srcId}\"'
-    print(api_url)
+    #Calling the uncorrelated function
     uncorrelatedaccounts(api_url,token)
